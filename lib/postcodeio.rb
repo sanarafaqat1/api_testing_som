0d401e8998905f349df2aa7473da2bfed62e580b
Sana Rafaqat <sanarafaqat041@gmail.com>
2018-11-28 10:41:07 +0000

    V.0.0.2 Created a services folder with single and multiple

---
+++ lib/postcodeio.rb
@@ -0,0 +1,15 @@
+#Inclde in the services
+require_relative 'services/single_postcode_service'
+require_relative 'services/multiple_postcodes_service'
+
+#Superclass
+class Postcodesio
+
+  def single_postcode_service
+
+  end
+
+  def multiple_postcodes_service
+
+  end
+end
